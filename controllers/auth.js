b92668c0c6bf2b5c1052a16962425b281ecaad9f
pecasi <pcastejon@borino.es>
2026-01-13 14:54:12 +0100

    restserver listo 13/01/2026 14:53

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response, request } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generateJWT } = require('../helpers/jwt-generate');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req = request, res = response) => {
     const { email, password } = req.body;
@@ -45,7 +46,53 @@ const login = async(req = request, res = response) => {
         });
     }   
 }
+const googleSignIn = async(req = request, res = response) => {
+    const { id_token } = req.body;  
+    try {
+        
+        const { name, picture, email } = await googleVerify(id_token);
+
+        //Verificar si el usuario existe
+        let usuario = await Usuario.findOne( { email } );
+
+        if ( !usuario ) {                   
+            //Crear usuario
+            const data = {
+                nombre: name,
+                email,
+                password: ':P',
+                img: picture,
+                google: true
+            };
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+        //Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador - Usuario bloqueado'
+            });
+        }   
+
+        //Generar el JWT
+        const token = await generateJWT( usuario.id );
+
+        res.json({
+            msg: 'Google Sign-In OK',
+            token,
+            id_token,
+            usuario
+        });
+    }       
+    catch (error) {
+        console.log(error);
+        return res.status(400).json({
+            msg: 'Hable con el administrador - El token no se pudo verificar'
+        });
+    }   
+}       
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
---
+++ helpers/google-verify.js
@@ -0,0 +1,27 @@
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+async function googleVerify(token = '') {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID,  // Specify the WEB_CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[WEB_CLIENT_ID_1, WEB_CLIENT_ID_2, WEB_CLIENT_ID_3]
+    });
+    const { name, email, picture } = ticket.getPayload();
+    // This ID is unique to each Google Account, making it suitable for use as a primary key
+    // during account lookup. Email is not a good choice because it can be changed by the user.
+    //   const userid = payload['sub'];
+    // If the request specified a Google Workspace domain:
+    // const domain = payload['hd'];
+
+    return { 
+        name, 
+        email, 
+        picture 
+    };
+}
+// verify().catch(console.error);
+
+module.exports = {
+    googleVerify
+};
