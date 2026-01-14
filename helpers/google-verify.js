698535a1fbb0566427f5173adfa98348a96b544f
pecasi <pcastejon@borino.es>
2026-01-14 14:38:32 +0100

    restserver listo 14/01/2026 14:36

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -1,12 +1,18 @@
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-async function googleVerify(token = '') {
+
+async function googleVerify(token = '') 
+{
+    console.log('Verifying token:', token);
+    console.log('Using GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
     const ticket = await client.verifyIdToken({
         idToken: token,
         audience: process.env.GOOGLE_CLIENT_ID,  // Specify the WEB_CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[WEB_CLIENT_ID_1, WEB_CLIENT_ID_2, WEB_CLIENT_ID_3]
     });
+    console.log('Token verified successfully');
+
     const { name, email, picture } = ticket.getPayload();
     // This ID is unique to each Google Account, making it suitable for use as a primary key
     // during account lookup. Email is not a good choice because it can be changed by the user.
